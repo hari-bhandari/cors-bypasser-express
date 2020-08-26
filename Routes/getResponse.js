17841637a16722a0a5cdfbe5b16fb404e2555246
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-08-26 18:16:34 +0100

    added get functionality to bypass cors

--- Routes/getResponse.js
+++ Routes/getResponse.js
@@ -15,5 +15,37 @@ router.post('/',async(req,res)=>{
     }
     res.status(200).send(response.data)
 })
-
+router.get('/',async(req,res)=>{
+    let response;
+    try {
+        response = await axios.get(req.query.url);
+    } catch (err) {
+        res.status(500).json({error:'Something went wrong'})
+    }
+    res.status(200).send(response.data)
+})
+// router.put('/',async(req,res)=>{
+//     delete req.headers.host
+//     const config = {
+//         headers: req.headers
+//     };
+//     const formData = req.body;
+//     let response;
+//     try {
+//         response = await axios.put(req.query.url, formData, config);
+//     } catch (err) {
+//         res.status(500).json({error:'Something went wrong'})
+//     }
+//     res.status(200).send(response.data)
+// })
+//
+// router.delete('/',async(req,res)=>{
+//     let response;
+//     try {
+//         response = await axios.delete(req.query.url);
+//     } catch (err) {
+//         res.status(500).json({error:'Something went wrong'})
+//     }
+//     res.status(200).send(response.data)
+// })
 module.exports=router;
