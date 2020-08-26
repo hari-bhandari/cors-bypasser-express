dda844ee6688daff46e65403257be9568748bbdd
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-08-26 11:15:26 +0100

    adding cors support to my site

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express=require('express')
+const cors=require('cors')
 ////////////////////////////////
 const app=express()
 ////////////////////////////////
 app.set('trust proxy',true);
 const PORT= process.env.PORT||5000
-
+//sadding cors support
+app.use(cors())
 
 const server=app.listen(PORT,console.log(`server running in Production mode on port ${PORT}`))
 
