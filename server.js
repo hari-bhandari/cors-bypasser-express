4ce9d404c60392e05755422d2a04b2d942aca56a
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-08-26 11:52:35 +0100

    implementing routes

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ dotenv.config({path:'./config/config.env'})
 
 app.set('trust proxy',true);
 const PORT= process.env.PORT||5000
-//sadding cors support
+//adding cors support
 app.use(cors())
-
+//implementing routes
+app.use('/ip',require('./Routes/getResponse'))
 const server=app.listen(PORT,console.log(`server running in Production mode on port ${PORT}`))
 
 //handle unhandled promised rejections
