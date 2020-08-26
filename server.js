6c669727ef734763ecc71782f3c397ea503e17e1
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-08-26 11:19:45 +0100

    :wrench: setting up config and added PORT config

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express=require('express')
 const cors=require('cors')
+const dotenv=require('dotenv')
 ////////////////////////////////
 const app=express()
 ////////////////////////////////
+app.set('trust proxy',true);
+dotenv.config({path:'./config/config.env'})
+
 app.set('trust proxy',true);
 const PORT= process.env.PORT||5000
 //sadding cors support
