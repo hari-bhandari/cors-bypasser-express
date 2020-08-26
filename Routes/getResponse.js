6a3767673eeb0233c5a6a89ada902f8519e45056
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-08-26 18:17:03 +0100

    added delete functionality to bypass cors

--- Routes/getResponse.js
+++ Routes/getResponse.js
@@ -24,20 +24,20 @@ router.get('/',async(req,res)=>{
     }
     res.status(200).send(response.data)
 })
-// router.put('/',async(req,res)=>{
-//     delete req.headers.host
-//     const config = {
-//         headers: req.headers
-//     };
-//     const formData = req.body;
-//     let response;
-//     try {
-//         response = await axios.put(req.query.url, formData, config);
-//     } catch (err) {
-//         res.status(500).json({error:'Something went wrong'})
-//     }
-//     res.status(200).send(response.data)
-// })
+router.put('/',async(req,res)=>{
+    delete req.headers.host
+    const config = {
+        headers: req.headers
+    };
+    const formData = req.body;
+    let response;
+    try {
+        response = await axios.put(req.query.url, formData, config);
+    } catch (err) {
+        res.status(500).json({error:'Something went wrong'})
+    }
+    res.status(200).send(response.data)
+})
 //
 // router.delete('/',async(req,res)=>{
 //     let response;
